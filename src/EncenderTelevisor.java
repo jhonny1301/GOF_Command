1cc400eb55c6bbc726b04158a4dee97ef9da8d52
JHONNY <jhonatangarrido.13@gmail.com>
2024-09-18 18:01:50 -0500

    V1.2

---
+++ src/EncenderTelevisor.java
@@ -0,0 +1,11 @@
+class EncenderTelevisor implements Comando {
+    private Televisor televisor;
+
+    public EncenderTelevisor(Televisor televisor) {
+        this.televisor = televisor;
+    }
+
+    public void ejecutar() {
+        televisor.encender();
+    }
+}
