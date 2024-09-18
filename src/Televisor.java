a68e2440efa93c967b3965616e7e4be9a128b0c3
JHONNY <jhonatangarrido.13@gmail.com>
2024-09-18 18:02:13 -0500

    V1.3

---
+++ src/Televisor.java
@@ -0,0 +1,9 @@
+class Televisor {
+    public void encender() {
+        System.out.println("El televisor está encendido.");
+    }
+
+    public void apagar() {
+        System.out.println("El televisor está apagado.");
+    }
+}
